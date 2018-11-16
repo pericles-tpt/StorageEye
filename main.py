743731b9d6e642cc8129a324600d8542db83e8de
anthonyklepac <43511844+anthonyklepac@users.noreply.github.com>
2018-11-16 13:03:14 +1100

    Added print_directory()
    Print the given directory object, to assist debugging

--- main.py
+++ main.py
@@ -3,25 +3,34 @@ from os.path import join, getsize
 from classes import Directory, File
 import sys
 
-def build_Directory(directoryName):
+def build_directory(directoryName):
     directory = Directory(directoryName)
 
     for (dirpath, childdirs, filenames) in walk(directory.name): 
 
         for filename in filenames:
             directory.files.append(File(filename, getsize(join(dirpath, filename))))
-
         for childdir in childdirs:
-            directory.children.append(build_Directory(join(dirpath, childdir)))
+            directory.children.append(build_directory(join(dirpath, childdir)))
 
         break
-    
     return directory
 
-if __name__ == "__main__":
-    path = ""
-    rootDirectory = build_Directory(path)
+def print_directory(directory, depth=0):
+    whitespace = 4*' '
 
+    print("{}{}\n".format(depth*whitespace, directory.name))
+
+    for file in directory.files:
+        print("{}{}\n".format((depth+1)*whitespace, file.name))
+    for child in directory.children:
+        print_directory(child, depth+1)
+
+
+if __name__ == "__main__":
+    path = r"c:\users\anthony.klepac\desktop\test folder"
+    rootDirectory = build_directory(path)
+    print_directory(rootDirectory)
 
 
     
