1c22dad362caa3cd72d4fef374486fc15da7c7a3
Pericles Telemachou <pericles@outlook.com.au>
2019-04-24 22:40:25 +1000

    Still working on refactoring, issue now with BuildDirectory class

--- classes.py
+++ classes.py
@@ -1,93 +1,125 @@
+from os import walk, lstat
+from os.path import join, getsize, lexists
+from platform import system
 import sys
 
-class Directory:
-    def __init__(self, directoryName=sys.path[0]):
+
+class InitDirectory:
+    def __init__(self, directoryName):
         self._name = directoryName
-        self._children = []
-        self._files = []
+        self.children = []
+        self.files = []
         self._size = 0
 
-        @property
-        def name(self):
-            return self._name
+    # Attribute encapsulation
+    @property
+    def name(self):
+        return self._name
 
-        @name.setter
-        def name(self, new_name):
-            self._name = new_name
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
-        @property
-        def size(self):
-            return self._size
+    @property
+    def size(self):
+        return self._size
 
-        @size.setter
-        def size(self, new_size):
-            self._size = new_size
+    @size.setter
+    def size(self, new_size):
+        self._size = new_size
 
-        @property
-        def dirpath(self):
-            return self._dirpath
 
-        def print_directory(directory=self, depth=0):
+class BuildDirectory:
+    def __init__(self, directoryName):
+        self._name = directoryName
 
-            whitespace = 4*' ' 
-            print("{}{}\n".format(depth*whitespace, directory.name))
+    @property
+    def name(self):
+        return self._name
 
-            for file in directory.files:
-                print("{}{}\n".format((depth+1)*whitespace, file.name))
-            for child in directory.children:
-                self.print_directory(child, depth+1)
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
-        def print_directory_file(directory=self, depth=0):
+    def build_directory_OS_exceptions(self, directory, childdir, dirpath):
+        # Mac
+        # PROBLEM: Folders have to be excluded from scan to stop scan taking upwards of 30 minutes
+        # FIX: Figure out how deep a scan of these folders needs to be (if they need to be scanned at all)
+        # to get useful information for the user and form there do a scan to a certain depth of the folders
+        cd_except_mac = ['Volumes', 'EFIROOTDIR', 'bin', 'sbin', 'System', 'Library', 'usr']
 
-            print(("{}{}|{}\n".format(str(depth), directory.name, str(directory.size))), file=dirlog_original)
+        if system() == "Darwin":
 
-            for file in directory.files:
-                print(str(depth) + file.name + '|' + str(file.size), file=dirlog_original)
-            for child in directory.children:
-                self.print_directory_file(child, depth+1)
+            cd_except_chosen = cd_except_mac
 
-        def build_directory(directoryName=self.name):
-            for (dirpath, childdirs, filenames) in walk(directory.name): 
+        else:
 
-                for filename in filenames:
+            cd_except_chosen = []
 
-                    directory.files.append(File(filename, getsize(join(dirpath, filename))))
-                    directory.size += getsize(directory.name + '/' + filename)
+        if childdir not in cd_except_chosen: 
 
-                for childdir in childdirs:
+            try:
+                directory.children.append(self.build_directory_template(join(dirpath, childdir)))
+                directory.size += getsize(childdir)
+            except PermissionError: 
+                pass
+            except FileNotFoundError: 
+                pass
+            except OSError: 
+                pass
 
-                    directory.size += getsize(directory.name + '/' + childdir)
+        else:
 
-                    # Mac
-                    # Folders excepted from scan are current stop-gap solution to scan taking upwards of 30 minutes
+            pass
 
-                    # FIX: Figure out how deep a scan of these folders needs to be (if they need to be scanned at all)
-                    # to get useful information for the user and form there do a scan to a certain depth of the folders
-                    if system() == "Darwin":
-                        if (childdir != 'Volumes' and childdir != 'EFIROOTDIR' and childdir != 'bin' and childdir != 'sbin' 
-                            and childdir != 'System' and childdir != 'Library' and childdir != 'usr'): 
-                            try:
-                                directory.children.append(build_directory(join(dirpath, childdir)))
-                                directory.size += getsize(childdir)
-                            except PermissionError: 
-                                pass
-                            except FileNotFoundError: 
-                                pass
-                            except OSError: 
-                                pass
-                        else:
-                            pass
+    def build_directory_template(self, directoryName):
+        directory = InitDirectory(directoryName)
 
-                    # Windows and Linux (maybe...)           
-                    else:
-                        directory.children.append(build_directory(join(dirpath, childdir)))
-                        directory.size += getsize(childdir)
+        for (dirpath, childdirs, filenames) in walk(directory.name): 
 
-                break
+            for filename in filenames:
+                
+                directory.files.append(File(filename, getsize(join(dirpath, filename))))
+                directory.size += getsize(directory.name + '/' + filename)
 
-            return directory
+            for childdir in childdirs:
 
-        self.build_directory()
+                directory.size += getsize(directory.name + '/' + childdir)
+                self.build_directory_OS_exceptions(directory, childdir, dirpath)
+
+            break
+
+        return directory
+
+
+class PrintDirectory:
+    def __init__(self, w_file=None):
+        self.file = w_file
+
+    def terminal(self, directory, depth=0):
+        whitespace = 4*' ' 
+        print("{}{}\n".format(depth*whitespace, self.directory.name))
+
+        for file in self.directory.files:
+
+            print("{}{}\n".format((depth+1)*whitespace, file.name))
+
+        for child in self.directory.children:
+
+            self.terminal(child, depth+1)
+
+    def txt(self, directory, depth=0):
+        print(("{}{}|{}\n".format(str(depth), self.directory.name, str(self.directory.size))), file=self.file)
+
+        for file in self.directory.files:
+
+            print("DOING SOMETHING")
+            print(str(depth) + file.name + '|' + str(file.size), file=self.file)
+
+        for child in self.directory.children:
+
+            print("DOING SOMETHING")
+            self.txt(child, depth+1)
 
 
 class File:
@@ -95,20 +127,19 @@ class File:
         self.name = name
         self.size = size
 
-        @property
-        def name(self):
-            return self._name
+    # Attribute encapsulation
+    @property
+    def name(self):
+        return self._name
 
-        @name.setter
-        def name(self, new_name):
-            self._name = new_name
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
-        @property
-        def size(self):
-            return self._size
+    @property
+    def size(self):
+        return self._size
 
-        @size.setter
-        def size(self, new_size):
-            self._size = new_size
-
-    
+    @size.setter
+    def size(self, new_size):
+        self._size = new_size
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from classes import Directory
+from classes import InitDirectory, BuildDirectory, PrintDirectory, File
+from classes import PrintDirectory
+from os import walk
+from os.path import join, getsize
+from platform import system
+import sys
+
+write = open("dirlog_original.txt", "a+")
 
 if __name__ == "__main__":
-    path = ""
-    directory = Directory()
-    directory.print_directory()
+    path = "/"
+    rootDirectory = BuildDirectory(path).build_directory_template(path)
+    printd = PrintDirectory(write)
+    printd.txt(rootDirectory)
     
