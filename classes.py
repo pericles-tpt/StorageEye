c0de147ff940e5451bb4cda77d2009b999075f2d
Pericles Telemachou <pericles@outlook.com.au>
2019-04-24 12:42:44 +1000

    Encapsulated class attributes to give us greater control of setting and getting them

--- classes.py
+++ classes.py
@@ -1,11 +1,46 @@
 class Directory:
     def __init__(self, directoryName):
-        self.name = directoryName
-        self.children = []
-        self.files = []
-        self.size = 0
+        self._name = directoryName
+        self._children = []
+        self._files = []
+        self._size = 0
+
+        @property
+        def name(self):
+            return self._name
+
+        @name.setter
+        def name(self, new_name):
+            self._name = new_name
+
+        @property
+        def size(self):
+            return self._size
+
+        @size.setter
+        def size(self, new_size):
+            self._size = new_size
+
 
 class File:
     def __init__(self, name, size):
         self.name = name
         self.size = size
+
+        @property
+        def name(self):
+            return self._name
+
+        @name.setter
+        def name(self, new_name):
+            self._name = new_name
+
+        @property
+        def size(self):
+            return self._size
+
+        @size.setter
+        def size(self, new_size):
+            self._size = new_size
+
+    
